b450338f64d5410850450ae1e2e8cf94d7b6e6c0
Ibrahim <ibrolarry2003@yahoo.com>
2022-09-08 00:55:26 +0100

    Labcom-Commerce

---
+++ frontend/src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+       <header>
+        <a href = '/'>Amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  )
+}
